4db0cd64d658fb3e44d3398040baf04b87519a16
impartial-cache-1887 <marciafreeman2011@gmail.com>
2019-09-28 02:37:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday,name) {
+  `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days) {
+  `It's ${days} days until ${holiday}!`
+}
